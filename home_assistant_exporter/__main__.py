8184a93a7807a103a35d8cf3e501871dedefba97
Michael Kuty <6du1ro.n@gmail.com>
2022-09-02 16:34:30 +0200

    Add home and cleanup

--- home_assistant_exporter/__main__.py
+++ home_assistant_exporter/__main__.py
@@ -60,9 +60,22 @@ async def start_cli() -> None:
         await connect(args, session)
 
 
-async def hello(request):
+async def metrics(request):
     metrics = generate_latest(registry)
     return web.Response(text=metrics.decode("utf-8"))
+
+
+async def home(request):
+    page = """
+        <html>
+        <head><title>Home Assistant Exporter</title></head>
+        <body>
+        <h1>Home Assistant Exporter</h1>
+        <p><a href="/metrics">Metrics</a></p>
+        </body>
+        </html>
+    """
+    return web.Response(text=page, content_type="text/html")
 
 
 def _get_labels(device):
@@ -84,10 +97,7 @@ def _get_labels(device):
     }
 
 
-async def init_hw_info(hass):
-    """
-    {'entity_id': 'sensor.mi10t_battery_power', 'state': '0.82', 'attributes': {'state_class': 'measurement', 'current': 0.19287, 'voltage': 4.24, 'unit_of_measurement': 'W', 'device_class': 'power', 'icon': 'mdi:battery-plus', 'friendly_name': 'mi10t Battery Power'}, 'last_changed': '2022-09-02T13:50:20.829672+00:00', 'last_updated': '2022-09-02T13:50:20.829672+00:00', 'context': {'id': '01GBZ7F04XVHB4BD2E43EZRPCG', 'parent_id': None, 'user_id': None}, 'area_id': None, 'config_entry_id': '598503b8cba66bf08613d2af749d1ba0', 'device_id': 'fa939d9a7d730fe3ccb23cbe015139b3', 'disabled_by': None, 'has_entity_name': False, 'entity_category': 'diagnostic', 'hidden_by': None, 'icon': None, 'name': 'mi10t', 'original_name': 'mi10t Battery Power', 'platform': 'mobile_app', 'configuration_url': None, 'config_entries': ['598503b8cba66bf08613d2af749d1ba0'], 'connections': [], 'entry_type': None, 'id': 'fa939d9a7d730fe3ccb23cbe015139b3', 'identifiers': [['mobile_app', '6eb682d56ab657e0']], 'manufacturer': 'Xiaomi', 'model': 'M2007J3SY', 'name_by_user': None, 'sw_version': '31', 'hw_version': None, 'via_device_id': No
-    """
+async def init_metrics(hass):
     for key, device in hass.get_all_entities().items():
         try:
             metric["hass_device_info"].labels(**_get_labels(device)).set(
@@ -96,29 +106,20 @@ async def init_hw_info(hass):
         except Exception as e:
             LOGGER.warning(f"Could not set {key} - {e}")
 
-        # LOGGER.info(f"State: {key} - {state}")
-
-    # for key, state in hass.states.items():
-    #     LOGGER.info(f"State: {key} - {state}")
-    # for key, entity in hass._entity_registry.items():
-    #     LOGGER.info(f"Entity: {key} - {entity}")
-    # for key, device in hass._device_registry.items():
-    #     LOGGER.info(f"Device: {key} - {device}")
-
 
 async def connect(args: argparse.Namespace, session: aiohttp.ClientSession) -> None:
     """Connect to the server."""
     async with HomeAssistantClient(args.url, args.token, session) as client:
         client.register_event_callback(log_events)
         await client._request_full_state()
-        await init_hw_info(client)
+        await init_metrics(client)
         await asyncio.sleep(360)
 
 
 def log_events(hass: HomeAssistantClient, event: str, event_data: dict) -> None:
     """Log node value changes."""
 
-    LOGGER.info("Received event: %s", event)
+    LOGGER.debug("Received event: %s", event)
     LOGGER.debug(event_data)
 
     if "old_state" and "new_state" in event_data:
@@ -136,7 +137,8 @@ def log_events(hass: HomeAssistantClient, event: str, event_data: dict) -> None:
 def main() -> None:
     """Run main."""
     app = web.Application()
-    app.add_routes([web.get("/", hello)])
+    app.add_routes([web.get("/", home)])
+    app.add_routes([web.get("/metrics", metrics)])
     loop = asyncio.new_event_loop()
 
     try:
